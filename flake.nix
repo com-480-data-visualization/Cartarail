caf6ce56a78f2989416d2d0dab7a79aa7b068b30
Yawen Guan <yawen.guan@proton.me>
2025-04-11 13:13:12 +0200

    env: Add typescript & Run formatter.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,40 @@
 {
-  description = "A nix-flake-based cfml2 development environment";
+  description = "A nix-flake-based development environment";
 
   inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = (with pkgs; [ python312Full ]) ++ (with pkgs.python312Packages; [ jupyter pandas numpy geopy ]);
-        };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              (with pkgs; [
+                python312Full
+                typescript
+              ])
+              ++ (with pkgs.python312Packages; [
+                jupyter
+                pandas
+                numpy
+                geopy
+              ]);
+          };
+        }
+      );
     };
 }
